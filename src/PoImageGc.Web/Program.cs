f4e2aa7e03058c16951b357b518c90e11d83d4c1
punkouter26 <punkouter26@gmail.com>
2026-01-16 18:14:07 -0500

    Fix UseStatusCodePagesWithReExecute API for preview SDK

--- src/PoImageGc.Web/Program.cs
+++ src/PoImageGc.Web/Program.cs
@@ -79,7 +79,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseStatusCodePagesWithReExecute("/not-found");
 app.UseHttpsRedirection();
 app.UseAntiforgery();
 
