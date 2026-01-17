33b02264c5ea08b76d7bda7b3868c36318277f1b
punkouter26 <punkouter26@gmail.com>
2026-01-17 14:09:24 -0500

    Switch to Aspire CLI for deployment instead of manual Docker/ACR build

--- src/PoImageGc.AppHost/Program.cs
+++ src/PoImageGc.AppHost/Program.cs
@@ -1,6 +1,15 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.PoImageGc_Web>("web")
+var web = builder.AddProject<Projects.PoImageGc_Web>("web")
     .WithExternalHttpEndpoints();
+
+// Configure production environment variables for Azure deployment
+// These will be set when deployed via Aspire CLI
+if (builder.ExecutionContext.IsPublishMode)
+{
+    web.WithEnvironment("AZURE_KEY_VAULT_ENDPOINT", "https://kv-poshared.vault.azure.net/")
+       .WithEnvironment("AZURE_CLIENT_ID", "183a4196-d24f-4519-afea-783ff01dbb17")
+       .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Production");
+}
 
 builder.Build().Run();
